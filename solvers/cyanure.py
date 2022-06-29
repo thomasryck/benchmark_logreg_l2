559efb71d3b4e408e625df9528d96a1ef2756532
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-06-29 17:54:20 +0200

    feat: test several solver + fix: same variable name

--- solvers/cyanure.py
+++ solvers/cyanure.py
@@ -16,9 +16,8 @@ class Solver(BaseSolver):
     requirements = ['cyanure']
 
     parameters = {
-        'solver': ['catalyst-miso', 'qning-miso', 'qning-ista',  'auto',  'acc-svrg'],
+        'solver': ['catalyst-miso', 'qning-miso', 'qning-ista',  'auto',  'acc-svrg']
     }
-
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
         if (scipy.sparse.issparse(self.X) and
@@ -29,18 +28,18 @@ class Solver(BaseSolver):
 
 
         self.solver_parameter = dict(
-        lambda_1=self.lmbd / self.X.shape[0], solver='auto', duality_gap_interval=10,
+        lambda_1=self.lmbd / self.X.shape[0], duality_gap_interval=10,
         tol=1e-12, verbose=False, solver=self.solver
         )
 
-        self.solver = estimators.Classifier(loss='logistic', penalty='l2',
+        self.solver_instance = estimators.Classifier(loss='logistic', penalty='l2',
                                        fit_intercept=False,
                         **self.solver_parameter)
        
 
     def run(self, n_iter):
-        self.solver.max_iter = n_iter
-        self.solver.fit(self.X, self.y)
+        self.solver_instance.max_iter = n_iter
+        self.solver_instance.fit(self.X, self.y)
 
     def get_result(self):
-        return self.solver.get_weights()
+        return np.squeeze(self.solver_instance.get_weights())
--- solvers/cyanure_old.py
+++ solvers/cyanure_old.py
@@ -7,7 +7,7 @@ with safe_import_context() as import_ctx:
 
 
 class Solver(BaseSolver):
-    name = 'Cyanure'
+    name = 'cyanure_old'
 
     install_cmd = 'conda'
     requirements = ['mkl', 'pip:cyanure-mkl']
@@ -22,16 +22,16 @@ class Solver(BaseSolver):
                 scipy.sparse.isspmatrix_csc(self.X)):
             self.X = scipy.sparse.csr_matrix(self.X)
 
-        self.solver = BinaryClassifier(loss='logistic', penalty='l2',
+        self.solver_instance = BinaryClassifier(loss='logistic', penalty='l2',
                                        fit_intercept=False)
         self.solver_parameter = dict(
-            lambd=self.lmbd / self.X.shape[0], solver='auto', it0=1000000,
+            lambd=self.lmbd / self.X.shape[0], it0=1000000,
             tol=1e-12, verbose=False, solver=self.solver
         )
 
     def run(self, n_iter):
-        self.solver.fit(self.X, self.y, max_epochs=n_iter,
+        self.solver_instance.fit(self.X, self.y, max_epochs=n_iter,
                         **self.solver_parameter)
 
     def get_result(self):
-        return self.solver.get_weights()
+        return self.solver_instance.get_weights()
