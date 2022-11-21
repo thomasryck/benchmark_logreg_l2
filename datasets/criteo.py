2bcb7aea434046c7b04fda9567dd89399b603f9f
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-11-21 14:38:35 +0100

    chore: lint criteo dataset

--- datasets/criteo.py
+++ datasets/criteo.py
@@ -17,19 +17,26 @@ class Dataset(BaseDataset):
     install_cmd = 'conda'
     requirements = ['pip:appdirs']
 
+    root_url = "http://pascal.inrialpes.fr/data2/cyanure/datasets"
+    x_url = root_url + "/criteo_X.npz"
+    y_url = root_url + "/criteo_y.npz"
+
     def __init__(self):
         self.X, self.y = None, None
 
     def get_data(self):
 
         if self.X is None:
-            cachedir = os.path.dirname(benchopt.__file__) + os.path.sep + "cache"
-            path_X = download("http://pascal.inrialpes.fr/data2/mairal/data/criteo_X.npz", os.path.join(cachedir, "criteo_X.npz"))
-            path_y = download("http://pascal.inrialpes.fr/data2/mairal/data/criteo_y.npz", os.path.join(cachedir, "criteo_y.npz"))
-            dataY=np.load(os.path.join(path_y), allow_pickle=True)
-            y=dataY['arr_0']
+            root_path = os.path.dirname(benchopt.__file__)
+            cachedir = root_path + os.path.sep + "cache"
+            path_X = download(self.x_url, 
+                              os.path.join(cachedir, "criteo_X.npz"))
+            path_y = download(self.y_url, 
+                              os.path.join(cachedir, "criteo_y.npz"))
+            dataY = np.load(os.path.join(path_y), allow_pickle=True)
+            y = dataY['arr_0']
             self.X = scipy.sparse.load_npz(os.path.join(path_X))
-            self.y=np.squeeze(y)
+            self.y = np.squeeze(y)
 
         data = dict(X=self.X, y=self.y)
 
